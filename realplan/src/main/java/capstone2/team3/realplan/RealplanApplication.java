23f76ac7f959ab3dcef1dc89d15f81f34dd58761
sejin <potatobanana050129@gmail.com>
2026-03-29 20:38:51 +0900

    Init Spring Boot project

---
+++ realplan/src/main/java/capstone2/team3/realplan/RealplanApplication.java
@@ -0,0 +1,13 @@
+package capstone2.team3.realplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealplanApplication.class, args);
+	}
+
+}
---
+++ realplan/src/test/java/capstone2/team3/realplan/RealplanApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone2.team3.realplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
