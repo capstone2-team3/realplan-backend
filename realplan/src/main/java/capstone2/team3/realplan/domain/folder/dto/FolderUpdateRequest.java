a4a86d44813151b82c96e2e665f852fb03c4010a
Anna Lee <anna8595@cau.ac.kr>
2026-06-03 00:52:07 +0900

    feat: add task api

--- realplan/src/main/java/capstone2/team3/realplan/domain/folder/dto/FolderUpdateRequest.java
+++ realplan/src/main/java/capstone2/team3/realplan/domain/folder/dto/FolderUpdateRequest.java
@@ -1,5 +1,6 @@
 package capstone2.team3.realplan.domain.folder.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -7,8 +8,10 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "폴더 수정 요청")
 public class FolderUpdateRequest {
 
+    @Schema(example = "전공 과제")
     @NotBlank(message = "폴더 이름은 필수입니다.")
     @Size(max = 50, message = "폴더 이름은 50자 이하여야 합니다.")
     private String name;
